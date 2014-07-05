85971afd4e5da86027f758aa076a58789391f4a9
PenguinDad <tamarok40@gmail.com>
2014-07-05 23:08:36 +0200

    Some attempts to improve performance

--- init.lua
+++ init.lua
@@ -3,17 +3,17 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 -- Copyright (c) 2014 PenguinDad
 _ = {}
--- Entity definition
-local entity = {
+
+-- Register the entity
+minetest.register_entity("freeze:sticky", {
 	physical = false,
 	collisionbox = {0, 0, 0, 0, 0, 0},
 	is_visible = false,
-}
-function entity:on_activate(staticdata)
-	self.object:set_armor_groups({immortal=1})
-end
--- Register the entity
-minetest.register_entity("freeze:sticky", entity)
+	on_activate = function(self, staticdata)
+		self.object:set_armor_groups({immortal=1})
+	end
+})
+
 minetest.register_chatcommand("freeze", {
 	params = "<name>",
 	description = "Freeze player <name>",
@@ -30,7 +30,7 @@ minetest.register_chatcommand("freeze", {
 			_[param].privs = minetest.get_player_privs(param)
 			local c = function(p) return p end
 			local tmp = c(_[param].privs)
-			tmp.interact = false
+			tmp.interact = nil
 			minetest.set_player_privs(param, tmp)
 			minetest.auth_reload()
 			tmp = nil
@@ -38,9 +38,11 @@ minetest.register_chatcommand("freeze", {
 				player:set_hp(1)
 			end
 			minetest.chat_send_all(param .. " was frozen by " .. name .. ".")
+			minetest.debug(param .. " was frozen at " .. minetest.pos_to_string(vector.round(player:getpos())))
 		end
 	end,
 })
+
 minetest.register_chatcommand("unfreeze", {
 	params = "<name>",
 	description = "Unfreeze player <name>",
@@ -58,7 +60,17 @@ minetest.register_chatcommand("unfreeze", {
 				minetest.chat_send_player(param, "You aren't frozen anymore.")
 				-- Clear the player info in the table
 				_[param] = nil
+				minetest.debug(param .. " was molten at " .. minetest.pos_to_string(vector.round(player:getpos())))
 			end
 		end
 	end,
 })
+
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	if _[name] ~= nil then
+		_[name].entity:remove()
+		_[name] = nil
+		minetest.debug(name .. " was molten because it left.")
+	end
+end)
